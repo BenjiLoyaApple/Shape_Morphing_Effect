ee378f0fcf779dfec19b047d109c8191fe2e8ee7
Benji Loya <benjiloya.apple@gmail.com>
2024-07-31 18:17:53 +0500

    Initial Commit

---
+++ OnBoarding18/OnBoarding18App.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoarding18App.swift
+//  OnBoarding18
+//
+//  Created by Benji Loya on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnBoarding18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
